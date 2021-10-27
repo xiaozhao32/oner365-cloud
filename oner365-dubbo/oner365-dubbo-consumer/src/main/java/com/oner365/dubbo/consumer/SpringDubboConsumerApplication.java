e7d6114cd29cec59374ad84b0aa520e73c738cb9
zhaoyong <xiaozhao32@hotmail.com>
2021-10-27 13:45:44 +0800

    update dubbo serivce

--- oner365-dubbo/oner365-dubbo-consumer/src/main/java/com/oner365/dubbo/consumer/SpringDubboConsumerApplication.java
+++ oner365-dubbo/oner365-dubbo-consumer/src/main/java/com/oner365/dubbo/consumer/SpringDubboConsumerApplication.java
@@ -3,21 +3,20 @@ package com.oner365.dubbo.consumer;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Dubbo 消费者服务
+ * 
  * @author zhaoyong
  */
-
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableDubbo
 @EnableDiscoveryClient
 @ComponentScan(basePackages = { "com.oner365.dubbo" })
 public class SpringDubboConsumerApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(SpringDubboConsumerApplication.class, args);
     }
--- oner365-dubbo/oner365-dubbo-provider/src/main/java/com/oner365/dubbo/provider/SpringDubboProviderApplication.java
+++ oner365-dubbo/oner365-dubbo-provider/src/main/java/com/oner365/dubbo/provider/SpringDubboProviderApplication.java
@@ -2,13 +2,16 @@ package com.oner365.dubbo.provider;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Dubbo 提供者服务
+ * 
  * @author zhaoyong
  */
+@SpringBootApplication
 @EnableDubbo
 @EnableDiscoveryClient
 @ComponentScan(basePackages = { "com.oner365.dubbo" })
