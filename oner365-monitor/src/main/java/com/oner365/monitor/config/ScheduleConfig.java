1f343ba83bbd57a13c3ec9300f05bca3e1e3fb49
zhaoyong <xiaozhao32@hotmail.com>
2021-12-05 10:00:40 +0800

    优化

--- oner365-monitor/src/main/java/com/oner365/monitor/config/ScheduleConfig.java
+++ oner365-monitor/src/main/java/com/oner365/monitor/config/ScheduleConfig.java
@@ -1,14 +1,10 @@
-
 package com.oner365.monitor.config;
-
-import java.util.Properties;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.quartz.SchedulerFactoryBeanCustomizer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 /**
@@ -17,31 +13,18 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
  * @author zhaoyong
  */
 @Configuration
-public class ScheduleConfig {
+public class ScheduleConfig implements SchedulerFactoryBeanCustomizer {
 
-    @Bean
-    public SchedulerFactoryBean schedulerFactoryBean(DataSource dataSource) {
-        SchedulerFactoryBean factory = new SchedulerFactoryBean();
-        factory.setDataSource(dataSource);
+    @Autowired
+    private DataSource dataSource;
 
-        // quartz参数
-        YamlPropertiesFactoryBean bean = new YamlPropertiesFactoryBean();
-        bean.setResources(new ClassPathResource("quartz.yml"));
-        Properties prop = bean.getObject();
-
-        if (prop != null) {
-            factory.setQuartzProperties(prop);
-            factory.setSchedulerName(prop.getProperty("org.quartz.scheduler.instanceName"));
-        }
-        // 延时启动
-        factory.setStartupDelay(1);
-        factory.setApplicationContextSchedulerContextKey("applicationContextKey");
+    @Override
+    public void customize(SchedulerFactoryBean schedulerFactoryBean) {
+        schedulerFactoryBean.setDataSource(dataSource);
+        schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContextKey");
         // 可选，QuartzScheduler
-        // 启动时更新己存在的Job，这样就不用每次修改targetObject后删除qrtz_job_details表对应记录了
-        factory.setOverwriteExistingJobs(true);
+        schedulerFactoryBean.setOverwriteExistingJobs(true);
         // 设置自动启动，默认为true
-        factory.setAutoStartup(true);
-
-        return factory;
+        schedulerFactoryBean.setAutoStartup(true);
     }
 }
