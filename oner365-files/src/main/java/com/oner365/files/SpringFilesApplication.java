828c29d9b6b8485ce2dcd049fa9c25e1609bcd1d
zhaoyong <xiaozhao32@hotmail.com>
2022-01-23 15:36:08 +0800

    优化

--- oner365-files/src/main/java/com/oner365/files/SpringFilesApplication.java
+++ oner365-files/src/main/java/com/oner365/files/SpringFilesApplication.java
@@ -2,7 +2,6 @@ package com.oner365.files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -23,7 +22,7 @@ import com.github.tobato.fastdfs.FdfsClientConfig;
 @EnableDiscoveryClient
 @EnableFeignClients
 @ComponentScan({ "com.oner365.common", "com.oner365.files" })
-public class SpringFilesApplication extends SpringBootServletInitializer {
+public class SpringFilesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFilesApplication.class, args);
