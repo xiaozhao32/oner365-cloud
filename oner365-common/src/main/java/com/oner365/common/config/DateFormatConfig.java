658e49c32573a921562f5ae6da1da9dcc89fc05e
zhaoyong <xiaozhao32@hotmail.com>
2022-03-23 17:27:07 +0800

    修改初始化Bean对象

--- oner365-common/src/main/java/com/oner365/common/config/DateFormatConfig.java
+++ oner365-common/src/main/java/com/oner365/common/config/DateFormatConfig.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.oner365.common.jackson.JavaTimeModule;
@@ -24,20 +24,14 @@ import com.oner365.util.DateUtil;
 @Configuration
 @ConditionalOnClass({ ObjectMapper.class })
 @AutoConfigureBefore({ JacksonAutoConfiguration.class })
-public class DateFormatConfig {
-    
-    /**
-     * customizer
-     * @return Jackson2ObjectMapperBuilderCustomizer
-     */
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer customizer() {
-        return builder -> {
-            builder.locale(Locale.CHINA);
-            builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
-            builder.simpleDateFormat(DateUtil.FULL_TIME_FORMAT);
-            builder.modules(new JavaTimeModule());
-        };
-    }
-    
+public class DateFormatConfig implements Jackson2ObjectMapperBuilderCustomizer {
+
+  @Override
+  public void customize(Jackson2ObjectMapperBuilder builder) {
+    builder.locale(Locale.CHINA);
+    builder.timeZone(TimeZone.getTimeZone(ZoneId.systemDefault()));
+    builder.simpleDateFormat(DateUtil.FULL_TIME_FORMAT);
+    builder.modules(new JavaTimeModule());
+  }
+
 }
