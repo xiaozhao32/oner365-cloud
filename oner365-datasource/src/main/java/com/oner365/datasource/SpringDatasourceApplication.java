45defef3897427ee1b12f03a1b574628e82b822f
zhaoyong <zhaoyong@cncytech.com>
2022-10-12 10:31:30 +0800

    update

--- oner365-datasource/src/main/java/com/oner365/datasource/SpringDatasourceApplication.java
+++ oner365-datasource/src/main/java/com/oner365/datasource/SpringDatasourceApplication.java
@@ -6,14 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-
 /**
  * Datasource 服务
  * 
  * @author zhaoyong
  */
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @ComponentScan({ "com.oner365.common", "com.oner365.datasource" })
