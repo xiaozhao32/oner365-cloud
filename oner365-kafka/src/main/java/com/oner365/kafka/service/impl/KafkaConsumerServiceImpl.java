51782f6c9486691170bf0401a78cfd6fcf32f4b1
zhaoyong <zhaoyong@cncytech.com>
2022-11-23 20:54:53 +0800

    add kafka callback

--- oner365-kafka/src/main/java/com/oner365/kafka/service/impl/KafkaConsumerServiceImpl.java
+++ oner365-kafka/src/main/java/com/oner365/kafka/service/impl/KafkaConsumerServiceImpl.java
@@ -1,15 +1,18 @@
 package com.oner365.kafka.service.impl;
 
+import javax.annotation.Resource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import com.oner365.common.enums.ResultEnum;
 import com.oner365.kafka.config.properties.KafkaProperties;
 import com.oner365.kafka.service.IKafkaConsumerService;
-
-import javax.annotation.Resource;
 
 /**
  * IKafkaConsumerService实现类
@@ -19,23 +22,37 @@ import javax.annotation.Resource;
 @Service
 public class KafkaConsumerServiceImpl implements IKafkaConsumerService {
 
-  private final Logger logger = LoggerFactory.getLogger(IKafkaConsumerService.class);
+    private final Logger logger = LoggerFactory.getLogger(IKafkaConsumerService.class);
 
-  @Resource
-  private KafkaProperties kafkaProperties;
+    @Resource
+    private KafkaProperties kafkaProperties;
 
-  @Resource
-  private KafkaTemplate<String, Object> kafkaTemplate;
+    @Resource
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
-  @Override
-  public ResultEnum convertAndSend(Object message) {
-    try {
-      kafkaTemplate.send(kafkaProperties.getTopic(), message);
-      return ResultEnum.SUCCESS;
-    } catch (Exception e) {
-      logger.error("convertAndSend error:", e);
+    @Override
+    public ResultEnum convertAndSend(Object message) {
+        try {
+            ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(kafkaProperties.getTopic(),
+                    message);
+            future.addCallback(new ListenableFutureCallback<Object>() {
+
+                @Override
+                public void onSuccess(Object result) {
+                    logger.info("Kafka callback success:{}", result);
+                }
+
+                @Override
+                public void onFailure(Throwable e) {
+                    logger.error("Kafka callback error:", e);
+                }
+
+            });
+            return ResultEnum.SUCCESS;
+        } catch (Exception e) {
+            logger.error("convertAndSend error:", e);
+        }
+        return ResultEnum.ERROR;
     }
-    return ResultEnum.ERROR;
-  }
 
 }
