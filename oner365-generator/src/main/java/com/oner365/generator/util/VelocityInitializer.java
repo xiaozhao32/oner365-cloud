4cba282ff50cf667545acfea75d2711330b9eb57
zhaoyong <xiaozhao32@hotmail.com>
2023-09-23 23:38:02 +0800

    修改velocity升级后不能加载的问题

--- oner365-generator/src/main/java/com/oner365/generator/util/VelocityInitializer.java
+++ oner365-generator/src/main/java/com/oner365/generator/util/VelocityInitializer.java
@@ -28,10 +28,9 @@ public class VelocityInitializer {
         Properties p = new Properties();
         try {
             // 加载classpath目录下的vm文件
-            p.setProperty("file.resource.file.class",
-                    "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+            p.setProperty("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
             // 定义字符集
-            p.setProperty(org.apache.velocity.runtime.RuntimeConstants.ENCODING_DEFAULT, Charset.defaultCharset().name());
+            p.setProperty(Velocity.ENCODING_DEFAULT, Charset.defaultCharset().name());
             // 初始化Velocity引擎，指定配置Properties
             Velocity.init(p);
         } catch (Exception e) {
