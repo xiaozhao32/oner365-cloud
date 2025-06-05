eafe086da9bac03684099f1985f0185e6563c06e
zhaoyong <xiaozhao32@hotmail.com>
2025-06-05 10:23:44 +0800

    update keycloak

--- oner365-keycloak/src/main/java/com/oner365/keycloak/controller/ApiAdminController.java
+++ oner365-keycloak/src/main/java/com/oner365/keycloak/controller/ApiAdminController.java
@@ -1,9 +1,12 @@
 package com.oner365.keycloak.controller;
 
-import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.keycloak.KeycloakPrincipal;
 import org.keycloak.representations.AccessToken;
-import org.keycloak.representations.UserInfo;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,19 +36,24 @@ public class ApiAdminController extends BaseController {
      * UserInfo
      * 
      * @param authentication Keycloak Auth
-     * @return ResponseEntity
+     * @return Map<String, Object>
      */
     @GetMapping("/userinfo")
-    public ResponseEntity<UserInfo> getUserInfo(KeycloakAuthenticationToken authentication) {
-        AccessToken accessToken = authentication.getAccount().getKeycloakSecurityContext().getToken();
-
-        UserInfo userInfo = new UserInfo();
-        userInfo.setPreferredUsername(accessToken.getPreferredUsername());
-        userInfo.setEmail(accessToken.getEmail());
-        userInfo.setGivenName(accessToken.getGivenName());
-        userInfo.setFamilyName(accessToken.getFamilyName());
-
-        return ResponseEntity.ok(userInfo);
+    public Map<String, Object> getUserInfo(HttpServletRequest request) {
+        KeycloakPrincipal<?> principal = (KeycloakPrincipal<?>) request.getUserPrincipal();
+        if (principal == null) {
+            return null;
+        }
+        AccessToken accessToken = principal.getKeycloakSecurityContext().getToken();
+        if (accessToken != null) {
+            Map<String, Object> userInfo = new HashMap<>();
+            userInfo.put("userName", accessToken.getPreferredUsername());
+            userInfo.put("email", accessToken.getEmail());
+            userInfo.put("givenName", accessToken.getGivenName());
+            userInfo.put("familyName", accessToken.getFamilyName());
+            return userInfo;
+        }
+        return null;
     }
 
 }
